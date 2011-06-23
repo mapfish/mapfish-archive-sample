a2ede1200a117a8a424f9b1bf3b6925b7670aea8
elemoine <eric.lemoine@camptocamp.com>
2011-06-23 12:19:34 +0000

    sample - upgrade to MapFish 2.2
    git-svn-id: http://svn.mapfish.org/svn/mapfish/sample/trunk@3816 25cca10d-0837-0410-9e4f-92b931fda1bc

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(
     url='',
     install_requires=[
         "psycopg2>=2.2.0,<=2.2.99",
-        "mapfish>=2.0,<=2.0.99",
+        "mapfish>=2.0,<=2.2.99",
         "httplib2>=0.6.0,<=0.6.99",
         "Babel<=0.9.99",
         "TileCache>=2.10,<=2.10.99",
-        "GeoFormAlchemy>=0.2,<=0.2.99",
+        "GeoFormAlchemy>=0.2,<=0.3.99",
     ],
     #setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
